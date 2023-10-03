8b67298cb908da40ee770574cc75c4cf24f1bfbe
Andrey <voix@TSI-MacBook.local>
2023-10-03 17:14:04 +0200

    Logic of each method extracted intoseparate function

--- src/main/java/tmp/empty/responder/rest/ResponderController.java
+++ src/main/java/tmp/empty/responder/rest/ResponderController.java
@@ -1,5 +1,6 @@
 package tmp.empty.responder.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -21,8 +22,7 @@ public class ResponderController {
 
     private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-    @PostMapping("/**")
-    public ResponseEntity<List<Object>> logPostRequest(@RequestHeader Map<String, String> headerValue, @RequestBody(required = false) JsonNode requestbody) throws InterruptedException, URISyntaxException, IOException {
+    private ResponseEntity<List<Object>> getListResponseEntity(@RequestHeader Map<String, String> headerValue, @RequestBody(required = false) JsonNode requestbody) throws JsonProcessingException {
         LOG.info("Header: {}", headerValue);
         LOG.info("Request body: {}", requestbody);
         ObjectMapper mapper = new ObjectMapper();
@@ -43,54 +43,21 @@ public class ResponderController {
         response.add(0,responseHeaders);
         response.add(1, responseBody);
         return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(response);
+    }
+
+    @PostMapping("/**")
+    public ResponseEntity<List<Object>> logPostRequest(@RequestHeader Map<String, String> headerValue, @RequestBody(required = false) JsonNode requestbody) throws InterruptedException, URISyntaxException, IOException {
+        return getListResponseEntity(headerValue, requestbody);
     }
 
     @PatchMapping("/**")
     public ResponseEntity<List<Object>> logPatchRequest(@RequestHeader Map<String, String> headerValue, @RequestBody(required = false) JsonNode requestbody) throws InterruptedException, URISyntaxException, IOException {
-        LOG.info("Header: {}", headerValue);
-        LOG.info("Request body: {}", requestbody);
-        ObjectMapper mapper = new ObjectMapper();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        Map<String, JsonNode> responseBody = new HashMap<>();
-        Map<String, Map<String,String>> responseHeaders = new HashMap<>();
-        responseHeaders.put("Headers:", headerValue);
-        if (isNull(requestbody)) {
-            JsonNode emptyBody = mapper.readTree("{\"MSG:\":\"No body was provided\"}");
-            responseBody.put("Body: ", emptyBody);
-            System.out.println(emptyBody);
-        }
-        else {
-            responseBody.put("Body: ", requestbody);
-        }
-        List<Object> response = new ArrayList<>();
-        response.add(0,responseHeaders);
-        response.add(1, responseBody);
-        return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(response);
+        return getListResponseEntity(headerValue, requestbody);
     }
 
     @PutMapping("/**")
     public ResponseEntity<List<Object>> logPutRequest(@RequestHeader Map<String, String> headerValue, @RequestBody(required = false) JsonNode requestbody) throws InterruptedException, URISyntaxException, IOException {
-        LOG.info("Header: {}", headerValue);
-        LOG.info("Request body: {}", requestbody);
-        ObjectMapper mapper = new ObjectMapper();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        Map<String, JsonNode> responseBody = new HashMap<>();
-        Map<String, Map<String,String>> responseHeaders = new HashMap<>();
-        responseHeaders.put("Headers:", headerValue);
-        if (isNull(requestbody)) {
-            JsonNode emptyBody = mapper.readTree("{\"MSG:\":\"No body was provided\"}");
-            responseBody.put("Body: ", emptyBody);
-            System.out.println(emptyBody);
-        }
-        else {
-            responseBody.put("Body: ", requestbody);
-        }
-        List<Object> response = new ArrayList<>();
-        response.add(0,responseHeaders);
-        response.add(1, responseBody);
-        return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(response);
+        return getListResponseEntity(headerValue, requestbody);
     }
 
     @GetMapping("/**")
